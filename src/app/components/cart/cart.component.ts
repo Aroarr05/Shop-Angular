620e01eceb00851a8221cb10d3b79facd752a0e3
Aroa <aroa.rivas.2003@gmail.com>
2025-02-18 20:23:47 +0100

    select en los precios

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -23,7 +23,7 @@ export class CartComponent {
   }
 }*/
 
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../../services/cart.service';
@@ -51,6 +51,45 @@ export class CartComponent {
 
   removeItem(product: Product) {
     this.cartService.removeFromCart(product);
+  }
+}*/
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../model/products';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrl: './cart.component.css',
+    standalone: false
+})
+export class CartComponent {
+  items: Product[] = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+    shipping: '' // Add shipping to the form group
+  });
+  shippingCosts!: Observable<{ type: string, price: number }[]>;
+
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {
+    this.shippingCosts = this.cartService.getShippingPrices();
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+  removeItem(product: Product) {
+    this.cartService.removeFromCart(product);
+    // Update the items array after removal
+    this.items = this.cartService.getItems();
   }
 }
 
